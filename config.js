709840096de60fa671e698b261c8420f27b259f7
devitgg99 <159687710+devitgg99@users.noreply.github.com>
2024-10-19 21:40:30 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Confessing",
+    introTitle: 'Hey oun i got something to ask you',
+    introDesc: ``,
+    btnIntro: 'Click This',
+    title: 'Will you be my GF oun????🥰',
+    desc: 'You are so cute and im so inlove with you right now',
+    btnYes: 'Yes Bong<333',
+    btnNo: 'Ort jong bleh',
+    question: 'Oun Prom men ort?',
+    btnReply: 'Submit',
+    reply: 'doysa bong eng sart chlart kur oy slanh',
+    mess: 'Okay Bong Slanh Oun na',
+    messDesc: '',
+    btnAccept: 'Okiiiii  Oun <3',
+    
+}
